987ae417e3a11ae1ff16d6b7827152ce77539bc7
Luka7711 <35857649+Luka7711@users.noreply.github.com>
2021-10-02 15:03:19 -0500

    returning size and inserting first element in linked list

--- exercises/linkedlist/index.js
+++ exercises/linkedlist/index.js
@@ -2,8 +2,34 @@
 // Implement classes Node and Linked Lists
 // See 'directions' document
 
-class Node {}
+class Node {
+  constructor(value, next) {
+    this.value = value;
+    this.next = next;
+  }
+}
 
-class LinkedList {}
+class LinkedList {
+  constructor() {
+    this.head = null;
+  }
 
-module.exports = { Node, LinkedList };
+  insertFirst(value) {
+    if (!this.head) this.head = new Node(value, null);
+    else {
+      let moveNode = this.head;
+      this.head = new Node(value, moveNode);
+    }
+  }
+
+  size() {
+    let lng = 0;
+    let currentNode = this.head;
+    while (currentNode) {
+      lng++;
+      currentNode = currentNode.next;
+    }
+    return lng;
+  }
+}
+// module.exports = { Node, LinkedList };
